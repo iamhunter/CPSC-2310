742f412e136ccb2d7550a20cd477fa08059dd7fc
iams3cond <andr3whunter@gmail.com>
2016-08-24 21:38:34 -0400

    Initial Commit

---
+++ lab01/firstprogram.m
@@ -0,0 +1,19 @@
+  label(start)
+
+     load(beta)
+     mul(gamma)
+     div(delta)
+     store(alpha)
+
+     halt
+
+  comment(`  data section for program  ')
+
+     word(alpha,50)
+     word(beta,2)
+     word(gamma,12)
+     word(delta,6)
+
+  comment(`  start execution at label start  ')
+
+     start
